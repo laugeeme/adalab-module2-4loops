f274480655590b4bb738dbc0c12591a931ba6f3a
laurageeme <registro.laura@gmail.com>
2020-03-07 17:59:14 +0100

    notes of preview classes

---
+++ _src/assets/js/03_form-design.js
@@ -0,0 +1,19 @@
+'use strict';
+
+console.log('hola');
+
+
+
+
+
+
+
+
+/* ----------------CLASES PREVIEW----------------:
+Color nombre-apellido: font__preview--title (en tipography). Por defecto: #114e4e
+Color puesto: font__preview--subtitle (en tipography). Por defecto: #54585a
+Color estilo tarjeta: preview__style--color  (en preview css). Por defecto: #438792;
+Color estilo botones: preview__card--icon (en preview css). Por defecto: #a2deaf;
+ */
+
+
