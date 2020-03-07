c160ba589e9712ba79b6df191ab865d8a67519a0
laurageeme <registro.laura@gmail.com>
2020-03-07 19:15:29 +0100

    extend label to include palette

--- _src/assets/js/03_form-design.js
+++ _src/assets/js/03_form-design.js
@@ -1,7 +1,6 @@
 'use strict';
 
-console.log('hola');
-
+console.log('hola design');
 
 
 
@@ -11,9 +10,23 @@ console.log('hola');
 
 /* ----------------CLASES PREVIEW----------------:
 Color nombre-apellido: font__preview--title (en tipography). Por defecto: #114e4e
-Color puesto: font__preview--subtitle (en tipography). Por defecto: #54585a
 Color estilo tarjeta: preview__style--color  (en preview css). Por defecto: #438792;
 Color estilo botones: preview__card--icon (en preview css). Por defecto: #a2deaf;
  */
 
 
+
+/* -- -- -- -- --CLASES DESIGN-- -- -- -- -- -- -
+
+INPUT RADIO: colorPalette1
+.colorBox__1 - 1
+.colorBox__1 - 2
+.colorBox__1 - 3
+.colorBox__2 - 1
+.colorBox__2 - 2
+.colorBox__2 - 3
+.colorBox__3 - 1
+.colorBox__3 - 2
+.colorBox__3 - 3
+.colorBox__4 - 3
+ */
