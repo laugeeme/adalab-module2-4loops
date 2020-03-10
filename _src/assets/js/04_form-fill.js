0c2f886eba565d02f4addd465510d538d818c3f0
mgarvia <maria.garvia.soler@gmail.com>
2020-03-10 18:55:19 +0100

    update js Celia with amendments name + surname

--- _src/assets/js/04_form-fill.js
+++ _src/assets/js/04_form-fill.js
@@ -1,33 +1,20 @@
-'use strict';
-console.log('hola');
-
+// 'use strict';
+// console.log('hola');
 // form-fill constants
-
 const fullName = document.querySelector('#name');
 const jobPosition = document.querySelector('#job');
 const emailAddress = document.querySelector('#email');
 const phoneNumber = document.querySelector('#phone');
 const linkedin = document.querySelector('#linkedin');
 const github = document.querySelector('#github');
-
 // preview constants
-
 const previewFullName = document.querySelector('.font__preview--title');
 const previewJobPosition = document.querySelector('.font__preview--subtitle');
-// const iconPhone = document.querySelector('.icon-phone');
-
-function copyInfo() {
-    previewFullName.innerHTML = fullName.value;
-    previewJobPosition.innerHTML = jobPosition.value;
+function copyInfoName(event) {
+    previewFullName.innerHTML = fullName.value || 'Nombre Apellido';
 }
-
-fullName.addEventListener('keydown', copyInfo);
-jobPosition.addEventListener('keydown', copyInfo);
-
-// function showIcon() {
-//     if (event.currentTarget) {
-
-//     }
-// }
-// phoneNumber.addEventListener('keyup', showIcon)
-
+function copyInfoJob() {
+    previewJobPosition.innerHTML = jobPosition.value || 'Puesto';
+}
+fullName.addEventListener('keyup', copyInfoName);
+jobPosition.addEventListener('keyup', copyInfoJob);
